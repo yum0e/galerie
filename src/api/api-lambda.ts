1a3d4fad8a52673234f50de9751dbd679c6b0bd8
bigq <yum0e@protonmail.com>
2024-01-10 17:02:11 +0100

    feat: introduce test for offline lambda

--- src/api/api-lambda.ts
+++ src/api/api-lambda.ts
@@ -1,11 +1,21 @@
 import { createHttpServer } from ".";
-import serverless from "serverless-http";
+import serverless, { Handler } from "serverless-http";
 import { logger } from "../logger";
 
+let cachedLambdaHandler: Handler;
+
+const getLambdaHandler = () => {
+  if (!cachedLambdaHandler) {
+    const httpServer = createHttpServer({
+      logger: logger.child({ component: "HttpAPIServer" }),
+    });
+    const app = httpServer.getApi();
+    cachedLambdaHandler = serverless(app, { provider: "aws" });
+  }
+  return cachedLambdaHandler;
+};
+
 export const handler = async (event: string, context: any) => {
-  const httpServer = createHttpServer({
-    logger: logger.child({ component: "HttpAPIServer" }),
-  });
-  const app = httpServer.getApi();
-  return serverless(app, { provider: "aws" })(event, context);
+  const lambdaHandler = getLambdaHandler();
+  return lambdaHandler(event, context);
 };
